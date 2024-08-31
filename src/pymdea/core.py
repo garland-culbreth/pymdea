9ba234a30785caefa48d6f65e999c30b3de99258
garland-culbreth <culbreth.garland@gmail.com>
2024-08-31 13:37:07 -0700

    use from import structure

--- src/pymdea/core.py
+++ src/pymdea/core.py
@@ -6,10 +6,10 @@ from typing import Literal, Self
 
 import numpy as np
 import polars as pl
-import stochastic.continuous
-import stochastic.noise
 from scipy import stats
 from scipy.optimize import curve_fit
+from stochastic.continuous import FractionalBrownianMotion
+from stochastic.noise import ColoredNoise, FractionalGaussianNoise, GaussianNoise
 from tqdm import tqdm
 
 
@@ -78,16 +78,16 @@ class DeaLoader:
 
         """
         if kind == "gn":
-            process = stochastic.noise.GaussianNoise()
+            process = GaussianNoise()
             self.data = process.sample(length)
         if kind == "cn":
-            process = stochastic.noise.ColoredNoise(beta=a)
+            process = ColoredNoise(beta=a)
             self.data = process.sample(length)
         if kind == "fgn":
-            process = stochastic.noise.FractionalGaussianNoise(hurst=a)
+            process = FractionalGaussianNoise(hurst=a)
             self.data = process.sample(length)
         if kind == "fbm":
-            process = stochastic.continuous.FractionalBrownianMotion(hurst=a)
+            process = FractionalBrownianMotion(hurst=a)
             self.data = process.sample(length)
         return self
 
